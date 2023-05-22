3236f33ea9025fff00423b366e190bd7dd2de06e
Duong Hanh Minh <minh.duong7438@hcmut.edu.vn>
2023-05-22 14:53:17 +0700

    Display data from Variables and JSON given

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './views/nav/Nav.js';
+// template + logic
+// const App = () => {}
+// JSX
+// Babel
 function App() {
+  let name ="Nathan";
+  let year = "2021";
+  let obj ={name: 'Nathan', year:'2023'};
+  let link = 'https://www.youtube.com/channel/UCaJPscRKdj5tCpJsWjKJlUA'
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        <h1>Hello world with React and {name} in {year}!</h1>
+        it is detail of Obj: {obj.name} in {obj.year}
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target='_blank'>Visit my channel</a>
       </header>
     </div>
   );
